16402ec95744903db03f12fe2b86e98c176d2c56
unknown <jotapemarra@hotmail.com>
2022-01-05 15:30:30 -0300

    Criada a primeira versão do app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import Comment from "./components/Comment/index";
+
 import './App.css';
 
 function App() {
+
+  const meuNome = "João Paulo Sant'Ana Marra";
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Comment />
+      <Comment />
+      <Comment />
+      <Comment />
     </div>
+
   );
 }
 
---
+++ src/components/Comment/index.jsx
@@ -0,0 +1,17 @@
+import "./style.css"
+
+function Comment (){
+    return (     
+    <div className="comment-item">
+        <div className="comment"> 
+          Lorem ipsum urna lacus aliquet vehicula fames sagittis gravida,
+          at aliquam lectus nam mollis nostra.
+        </div>
+        <div className="footer">
+          <b>João Paulo Sant Ana marra</b> <span>05/01/2022</span> 
+        </div>
+    </div>  
+    );
+}
+
+export default Comment;
