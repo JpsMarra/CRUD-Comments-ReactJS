d4f48f126bf8e11cfa08b55e1085e1131fc91916
unknown <jotapemarra@hotmail.com>
2022-01-06 13:13:34 -0300

    Migrado o App Comment para a Class Component e implementado o State

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
 import Comment from "./components/Comment/index";
-
 import './App.css';
+import React from "react";
 
-function App() {
+class App extends React.Component {
 
-  return (
+  state = {
+    comments: [
+      {
+        name: "João Paulo Sant Ana Marra",
+        email: "jotapemarra@hotmail.com",
+        date: new Date(),
+        message: "Passando parâmetros para os componentes React",
+      },
+      {
+        name: "Nome Teste",
+        email: "emailteste@email.com",
+        date: new Date(),
+        message: "Passando parâmetros de teste para os componentes React",
+      },
+      {
+        name: "João Paulo Sant Ana Marra",
+        email: "jotapemarra@hotmail.com",
+        date: new Date(),
+        message: "Passando parâmetros para os componentes React",
+      },
+      {
+        name: "Nome Teste",
+        email: "emailteste@email.com",
+        date: new Date(),
+        message: "Passando parâmetros de teste para os componentes React",
+      },
+    ],
+  };
 
-    <div className="App">
-      <h1>Comments App!</h1>
-      <Comment 
-      name="João Paulo Sant Ana Marra"
-      email="jotapemarra@hotmail.com"
-      date={new Date()}
-      message="Passando parâmetros para os componentes React"
-      />
-      <Comment 
-      name="Nome Teste"
-      email="emailteste@email.com"
-      date={new Date()}
-      message="Passando parâmetros de teste para os componentes React"
-      />
-    </div>
+  addComment=() => {
+    const newComment = {
+      name: "João",
+      email: "mail@mail.com",
+      date: new Date(),
+      message: "Adicionando comments"
+    };
 
-  );
-}
+      this.setState({
+        comments: [...this.state.comments, newComment]
+      });
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Comments App!</h1>
+        {this.state.comments.map((comment, index) => {
+          return (
+            <Comment
+              key={index}
+              name={comment.name}
+              email={comment.email}
+              date={comment.date}
+              message={comment.message}
+            />
+          );
+        })}
+        <button onClick={this.addComment}>Adicionar Comentário</button>
+      </div>
+    );
+  }
+  }
 
 export default App;
